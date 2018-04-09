4f5cbb9b7e3b6cdbf6072a4b57d676f0aa38bb86
denim emetrope <jerome.condere@gmail.com>
2018-04-09 16:48:46 +0200

    rm csv

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## A simple spark project
-csv file cant be found on kaggle
+csv file cant be found on kaggle, here:
 https://www.kaggle.com/therohk/million-headlines
--- src/main/scala/simple/spark/SimpleSpark.scala
+++ src/main/scala/simple/spark/SimpleSpark.scala
@@ -2,9 +2,8 @@ import org.apache.spark.sql.SparkSession
 
 object SimpleSpark {
   def main(args: Array[String]) {
-    val csvFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+  	val csvFile = "abcnews-date-text.csv" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Spark Application").getOrCreate()
 
     spark.stop()
   }
